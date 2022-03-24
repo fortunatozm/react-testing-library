474fb3d9345ac4543931dec75caaf5718069a05e
fortunatozm <fortunatompongo@hotmail.com>
2022-03-24 13:47:27 -0300

    5. Teste o componente <Pokedex.js /> - OK

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from './renderWithRouter';
+import pokemons from '../data';
 import App from '../App';
 
 describe('Teste o componente <Pokedex.js />', () => {
@@ -10,6 +11,7 @@ describe('Teste o componente <Pokedex.js />', () => {
     const testPokedexTitle = screen.getByRole('heading', { level: 2,
       name: 'Encountered pokémons' });
     expect(testPokedexTitle).toBeInTheDocument();
+    expect(testPokedexTitle.textContent).toBe('Encountered pokémons');
   });
 
   test('Teste se é exibido o próximo Pokémon da lista quando o botão Próximo '
@@ -17,26 +19,70 @@ describe('Teste o componente <Pokedex.js />', () => {
     renderWithRouter(<App />);
     const btText = screen.getByRole('button', { name: 'Próximo pokémon' });
     expect(btText).toBeInTheDocument();
-    userEvent.click(btText);
-    const btNext = screen.getByText('Charmander');
-    expect(btNext).toBeInTheDocument();
-    userEvent.click(btText);
-    userEvent.click(btText);
-    userEvent.click(btText);
-    userEvent.click(btText);
-    userEvent.click(btText);
-    userEvent.click(btText);
-    userEvent.click(btText);
-    userEvent.click(btText);
+    for (let index = 0; index < pokemons.length; index += 1) {
+      const btNext = screen.getByText(pokemons[index].name);
+      expect(btNext).toBeInTheDocument();
+      userEvent.click(btText);
+    }
     const btFirst = screen.getByText('Pikachu');
     expect(btFirst).toBeInTheDocument();
   });
 
   test('Teste se é mostrado apenas um Pokémon por vez.', () => {
     renderWithRouter(<App />);
-    const btClick = screen.getByRole('button', { name: 'Próximo pokémon' });
+    const btClick = screen.getByTestId('next-pokemon');
     userEvent.click(btClick);
     const btList = screen.getAllByTestId('pokemon-name');
     expect(btList.length).toBe(1);
+  });
+
+  test('Teste se a Pokédex tem os botões de filtro.', () => {
+    renderWithRouter(<App />);
+    const btId = screen.getAllByTestId('pokemon-type-button');
+    expect(btId[0].textContent).toBe('Electric');
+    userEvent.click(btId[0]);
+    const eletric = screen.getAllByText('Electric');
+    expect(eletric.length).toBe(2);
+
+    expect(btId[1].textContent).toBe('Fire');
+    userEvent.click(btId[1]);
+    const fire = screen.getAllByText('Fire');
+    expect(fire.length).toBe(2);
+
+    expect(btId[2].textContent).toBe('Bug');
+    userEvent.click(btId[2]);
+    const bug = screen.getAllByText('Bug');
+    expect(bug.length).toBe(2);
+
+    expect(btId[3].textContent).toBe('Poison');
+    userEvent.click(btId[3]);
+    const poison = screen.getAllByText('Poison');
+    expect(poison.length).toBe(2);
+
+    expect(btId[4].textContent).toBe('Psychic');
+    userEvent.click(btId[4]);
+    const psychic = screen.getAllByText('Psychic');
+    expect(psychic.length).toBe(2);
+
+    expect(btId[5].textContent).toBe('Normal');
+    userEvent.click(btId[5]);
+    const normal = screen.getAllByText('Normal');
+    expect(normal.length).toBe(2);
+
+    expect(btId[6].textContent).toBe('Dragon');
+    userEvent.click(btId[6]);
+    const dragon = screen.getAllByText('Dragon');
+    expect(dragon.length).toBe(2);
+
+    const allways = screen.getByText('All');
+    expect(allways).toBeInTheDocument();
+    userEvent.click(allways);
+
+    const proximo = screen.getByText('Próximo pokémon');
+    userEvent.click(proximo);
+    userEvent.click(proximo);
+    userEvent.click(proximo);
+    const pokemom = screen.getByText('Ekans');
+    expect(pokemom).toBeInTheDocument();
   });
 });
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <Pokemon.js />', () => {
+  test('Teste se é renderizado um card com as informações de determinado '
+  + 'pokémon.', () => {
+    renderWithRouter(<App />);
+    const pokemonPikachu = screen.getByText('Pikachu');
+    expect(pokemonPikachu).toBeInTheDocument();
+    const pokemonType = screen.getByTestId('pokemon-type');
+    expect(pokemonType.textContent).toBe('Electric');
+    const pokemonWeight = screen.getByText('Average weight: 6.0 kg');
+    expect(pokemonWeight).toBeInTheDocument();
+    const pokemonImg = screen.getByRole('img');
+    expect(pokemonImg.src).toBe('https://cdn2.bulbagarden.net/upload/b/b2/'
+    + 'Spr_5b_025_m.png');
+    expect(pokemonImg.alt).toBe('Pikachu sprite');
+  });
+
+  test('Teste se o card do Pokémon indicado na Pokédex contém um link de navegação para'
+  + 'exibirdetalhes deste Pokémon. O link deve possuir a URL /pokemons/<id>, onde <id> '
+  + 'é o id do Pokémon exibido;', () => {
+    const linkPokemom = screen.getByText('More details');
+    expect(linkPokemom.closest('a')).toHaveAttribute('href', 'pokemons/25');
+  });
+});
