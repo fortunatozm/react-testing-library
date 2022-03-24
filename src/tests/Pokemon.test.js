9fe19aa9f834b82caf3c58f2d88763bc87e43122
fortunatozm <fortunatompongo@hotmail.com>
2022-03-24 17:05:46 -0300

    6. Teste o componente <Pokemon.js /> - OK

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
 import renderWithRouter from './renderWithRouter';
 
 describe('Teste o componente <Pokemon.js />', () => {
@@ -22,7 +24,38 @@ describe('Teste o componente <Pokemon.js />', () => {
   test('Teste se o card do Pokémon indicado na Pokédex contém um link de navegação para'
   + 'exibirdetalhes deste Pokémon. O link deve possuir a URL /pokemons/<id>, onde <id> '
   + 'é o id do Pokémon exibido;', () => {
-    const linkPokemom = screen.getByText('More details');
-    expect(linkPokemom.closest('a')).toHaveAttribute('href', 'pokemons/25');
+    renderWithRouter(<App />);
+    const linkPokemom = screen.getByText('More details').closest('a');
+    expect(linkPokemom).toHaveAttribute('href', '/pokemons/25');
+  });
+
+  test('Teste se o card do Pokémon indicado na Pokédex contém um link de navegação para'
+  + 'exibirdetalhes deste Pokémon. O link deve possuir a URL /pokemons/<id>, onde <id> '
+  + 'é o id do Pokémon exibido;', () => {
+    const { history } = renderWithRouter(<App />);
+    const clickMore = screen.getByText('More details');
+    userEvent.click(clickMore);
+    const moreTitle = screen.getByRole('heading', { level: 2, name: 'Pikachu Details' });
+    expect(moreTitle).toBeInTheDocument();
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+  });
+
+  test('Teste se existe um ícone de estrela nos Pokémons favoritados.', () => {
+    renderWithRouter(<App />);
+    const textDetails = screen.getByText(/More details/i);
+    userEvent.click(textDetails);
+    const check = screen.getByRole('checkbox');
+    userEvent.click(check);
+    renderWithRouter(<FavoritePokemons />);
+    const iconFavo = screen.getByAltText('Pikachu is marked as favorite');
+    expect(iconFavo).toBeInTheDocument();
+    expect(iconFavo.src).toBe('http://localhost/star-icon.svg');
+
+    // userEvent.click(clickMore);
+    // const moreTitle = screen.getByRole('heading', { level: 2, name: 'Pikachu Details' });
+    // expect(moreTitle).toBeInTheDocument();
+    // const { pathname } = history.location;
+    // expect(pathname).toBe('/pokemons/25');
   });
 });
